660b0eb80bb233c85239fa8e4533293c2bf60d8c
HelenaLannes18 <helena.lannes.salles@hotmail.com>
2022-10-21 09:06:04 -0300

    deploy backend

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,24 @@
 import Layout from '../components/Layout';
+import { useEffect, useState } from 'react';
 import ProductItem from '../components/ProductItem';
-import data from '../utils/data';
+import axios from 'axios';
+//import data from '../utils/data';
 
-export default function Home() {
+export async function getStaticProps() {
+  const data = await fetch('http://localhost:4000/api/products');
+
+  const products = await data.json();
+
+  return {
+    props: { products },
+  };
+}
+
+export default function Home({ products }) {
   return (
     <Layout title="Home Page">
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <ProductItem product={product} key={product.slug}></ProductItem>
         ))}
       </div>
